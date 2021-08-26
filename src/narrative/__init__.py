1273de9a76d587859ff3dd2b21852520d0d0b1a2
David L. Day <dday376@gmail.com>
2021-08-26 06:45:02 -0400

    moved code to src

--- src/narrative/__init__.py
+++ src/narrative/__init__.py
@@ -6,8 +6,9 @@ import re
 
 import pkg_resources
 
-__version__ = pkg_resources.resource_string(
-    'narrative', 'VERSION').decode('utf-8').strip()
+__version__ = (
+    pkg_resources.resource_string("narrative", "VERSION").decode("utf-8").strip()
+)
 
 # NOTE: Smart quotes are error prone. I recommend converting
 # them to regular quotes first.
@@ -17,7 +18,7 @@ __version__ = pkg_resources.resource_string(
 # http://pythex.org/
 # http://www.metaltoad.com/blog/regex-quoted-string-escapable-quotes
 # DIALOGUE_RE = r'[\"“]((?:.(?![\"“]))*.?)[\"”\\n]'
-DIALOGUE_RE = re.compile(r'[\"“](?:.(?![\"“]))*.?[\"”\\n]', re.MULTILINE)
+DIALOGUE_RE = re.compile(r"[\"“](?:.(?![\"“]))*.?[\"”\\n]", re.MULTILINE)
 
 
 def get_dialogue(text, dialogue_regex=DIALOGUE_RE):
