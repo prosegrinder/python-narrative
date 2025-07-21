922003dbcbf377e2b2e130d86418a39adbb1144d
David L. Day <1132144+davidlday@users.noreply.github.com>
2025-07-20 20:01:58 -0400

    feat: add type hints and type marker (#47)

--- src/narrative/__init__.py
+++ src/narrative/__init__.py
@@ -6,12 +6,9 @@ A small Python package for splitting text into dialogue and narrative.
 """
 
 import re
-import sys
 
-if sys.version_info >= (3, 9):
-    from importlib import metadata
-else:
-    import importlib_metadata as metadata
+from importlib import metadata
+from typing import Final
 
 __version__ = metadata.version(__name__)
 
@@ -23,18 +20,25 @@ __version__ = metadata.version(__name__)
 # http://pythex.org/
 # http://www.metaltoad.com/blog/regex-quoted-string-escapable-quotes
 # DIALOGUE_RE = r'[\"“]((?:.(?![\"“]))*.?)[\"”\\n]'
-DIALOGUE_RE = re.compile(r"[\"“](?:.(?![\"“]))*.?[\"”\\n]", re.MULTILINE)
+DIALOGUE_RE: Final[re.Pattern] = re.compile(
+    r"[\"“](?:.(?![\"“]))*.?[\"”\\n]", re.MULTILINE
+)
 
-BRITISH_DIALOGUE_RE = re.compile(r"['‘](?:.(?!['‘]))*.?['’\n]", re.MULTILINE)
+BRITISH_DIALOGUE_RE: Final[re.Pattern] = re.compile(
+    r"['‘](?:.(?!['‘]))*.?['’\n]", re.MULTILINE
+)
 
 
-def get_dialogue(text, dialogue_regex=DIALOGUE_RE):
+def get_dialogue(text: str, dialogue_regex: re.Pattern = DIALOGUE_RE) -> list[str]:
     """Get the dialogue fragments from a piece of text
 
     Parameters:
     ----------
     text : str
         a block of english language text
+    dialogue_regex : re.Pattern, optional
+        a regular expression to match dialogue fragments,
+        by default DIALOGUE_RE
 
     Returns:
     -------
@@ -45,13 +49,16 @@ def get_dialogue(text, dialogue_regex=DIALOGUE_RE):
     return dialogue
 
 
-def get_narrative(text, dialogue_regex=DIALOGUE_RE):
+def get_narrative(text: str, dialogue_regex: re.Pattern = DIALOGUE_RE) -> list[str]:
     """Get the narrative fragments from a piece of text
 
     Parameters:
     ----------
     text : str
         a block of english language text
+    dialogue_regex : re.Pattern, optional
+        a regular expression to match dialogue fragments,
+        by default DIALOGUE_RE
 
     Returns:
     -------
@@ -62,13 +69,16 @@ def get_narrative(text, dialogue_regex=DIALOGUE_RE):
     return narrative
 
 
-def split(text, dialogue_regex=DIALOGUE_RE):
+def split(text: str, dialogue_regex: re.Pattern = DIALOGUE_RE) -> dict[str, list[str]]:
     """Split a piece of text into dialogue and narrative
 
     Parameters:
     ----------
     text : str
         a block of english language text
+    dialogue_regex : re.Pattern, optional
+        a regular expression to match dialogue fragments,
+        by default DIALOGUE_RE
 
     Returns:
     -------
